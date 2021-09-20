4a6229332c6767dcac0bce02c3a2df136cd12a1b
prithvi-staqo <77377286+prithvisingh-staqo@users.noreply.github.com>
2021-09-20 15:06:19 +0530

    modified

--- cron_tracker_data.sh
+++ cron_tracker_data.sh
@@ -1,4 +1,4 @@
 
 
 
-C:/Users/Administrator/Desktop/Tracker_listener/venv/scripts/python3.9 C:/Users/Administrator/Desktop/Tracker_listener/cron.py
+python C:/Users/Administrator/Desktop/Tracker_listener/cron.py
